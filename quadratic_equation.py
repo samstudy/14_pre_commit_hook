8932a394ebf6df699c6eaba1cf2944dbcb527203
Samat Karymshakov <bskordo@gmail.com>
2016-12-31 18:03:51 +0600

    Test case

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,4 +1,4 @@
-from math import sqrt
+'''from math import sqrt
 
 
 def get_roots(a, b, c):
@@ -7,6 +7,22 @@ def get_roots(a, b, c):
     root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant < 0:
     	return None, None
+    if discriminant == 0:
+        return root1, None
+    else:
+        return root1, root2'''
+
+
+from cmath import sqrt
+
+
+def get_roots(a, b, c):
+    discriminant = b ** 2 - 4 * a * c
+    if discriminant < 0:
+    	return None, None
+    else:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant == 0:
         return root1, None
     else:
